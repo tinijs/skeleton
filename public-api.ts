7d08a4d1b3e8952af3806c59a3964501b55ac2b7
Lam Nhan <nhan.lh@raksul.com>
2023-01-02 21:28:41 +0700

    Add vendor js

--- public-api.ts
+++ public-api.ts
@@ -1,19 +1,19 @@
 // app
-export {AppRoot} from './app/app';
+export * from './app/app';
 // ...
 
 // layouts
-export {LayoutDefault} from './layouts/default.layout';
+export * from './layouts/default.layout';
 // ...
 
 // pages
-export {PageHome} from './pages/home.page';
+export * from './pages/home.page';
 // ...
 
 // components
-export {AppHeader} from './components/header.component';
+export * from './components/header.component';
 // ...
 
 // services
-export {SampleService} from './services/sample.service';
+export * from './services/sample.service';
 // ...
