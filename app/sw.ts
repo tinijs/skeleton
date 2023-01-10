939bec235fe0c82d55f8737dc7982675acfa9c1a
Lam Nhan <nhan.lh@raksul.com>
2023-01-10 13:33:48 +0700

    Test tini pwa init

--- app/sw.ts
+++
@@ -1,7 +0,0 @@
-import {precacheAndRoute, self} from '@tinijs/pwa';
-
-// 0. Precaching (change the glob patterns in "../build-sw.js")
-precacheAndRoute(self.__WB_MANIFEST);
-
-// 1. Custom (advanced usecases)
-// ...
